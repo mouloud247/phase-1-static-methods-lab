5858a06d89f01e3a11681af92d772c0aca159e6b
Mouloud Mecheter <mouloudmecheter@gmail.com>
2023-09-09 19:52:19 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
 class Formatter {
   //add static methods here
+  static capitalize(toCapitalize) {
+    return toCapitalize.charAt(0).toUpperCase() + toCapitalize.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "")
+
+  }
+
+
+
+
+  static titleize(string) {
+    const exceptions = {
+      the: 'the',
+      a: 'a',
+      an: 'an',
+      but: 'but',
+      of: 'of',
+      and: 'and',
+      for: 'for',
+      at: 'at',
+      by: 'by',
+      from: 'from'
+    }
+    const words = string.split(' ');
+
+
+    const titleized = words.map(e => {
+
+      if (exceptions.hasOwnProperty(e)) {
+        return e;
+      } else {
+        return e.charAt(0).toUpperCase() + e.slice(1);
+
+      }
+    })
+
+    titleized[0] = titleized[0].charAt(0).toUpperCase() + titleized[0].slice(1);
+    return titleized.join(' ');
+  }
 }
